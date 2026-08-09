9ebf0db95214fc54c20e088bf0efa9db0bddf696
kaylasreynolds <kaylasreynolds@gmail.com>
2026-08-09 02:31:12 -0600

    Use appointment schedule manager

--- app/admin/events/[eventId]/schedule/page.tsx
+++ app/admin/events/[eventId]/schedule/page.tsx
@@ -1,1 +1,9 @@
-import{Schedule}from"../../EventConsole";export default async function Page({params}:{params:Promise<{eventId:string}>}){return <Schedule eventId={(await params).eventId}/>}
+import { ScheduleManager } from "../../ScheduleManager";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ eventId: string }>;
+}) {
+  return <ScheduleManager eventId={(await params).eventId} />;
+}
