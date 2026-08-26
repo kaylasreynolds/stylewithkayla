e8f718dc5d74f0d836681672c61e9faa442a51d2
kaylasreynolds <kaylasreynolds@gmail.com>
2026-08-26 17:41:20 -0600

    Match admin preview to standalone Style Summary shell

--- app/admin/recap-preview/[bookingId]/page.tsx
+++ app/admin/recap-preview/[bookingId]/page.tsx
@@ -2,6 +2,7 @@ import { getAdminEmails } from "@/lib/server/runtime";
 import { accessSignOutPath, requireAccessUser } from "../../../cloudflare-access";
 import RecapPreview from "./RecapPreview";
 import "../../../style-summary/style-summary.css";
+import "../../../style-summary/style-summary-shell.css";
 
 export const dynamic = "force-dynamic";
 export default async function Page({ params }: { params: Promise<{ bookingId: string }> }) {
