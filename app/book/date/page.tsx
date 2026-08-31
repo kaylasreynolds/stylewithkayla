3132ce55e6d7149925ba6485fdbd15d5fcd1bd96
Kayla Reynolds <kaylasreynolds@gmail.com>
2026-08-30 22:04:29 -0600

    Update back link to point to the home page

--- app/book/date/page.tsx
+++ app/book/date/page.tsx
@@ -250,7 +250,7 @@ export default function SingleDateBookingPage() {
   return (
     <main className={styles.page}>
       <header className={styles.header}>
-        <Link href="/" className={styles.backLink}>← Back</Link>
+        <Link href="https://stylewithkayla.com/index.html" className={styles.backLink}>← Back to Home</Link>
         <div className={styles.wordmark}>STYLE <span>WITH KAYLA</span><em>Personal Stylist</em></div>
         <span />
       </header>
